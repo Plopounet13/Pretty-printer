562d09fccd9fb142b976f8dcd7f8d52818181dcf
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2016-01-09 16:49:06 +0100

    first commit

---
+++ pretty-printer/pp.c
@@ -0,0 +1,9 @@
+//
+//  pp.c
+//  pretty-printer
+//
+//  Created by Loïs Paulin on 09/01/2016.
+//
+//
+
+#include "pp.h"
---
+++ pretty-printer/pp.h
@@ -0,0 +1,14 @@
+//
+//  pp.h
+//  pretty-printer
+//
+//  Created by Loïs Paulin on 09/01/2016.
+//
+//
+
+#ifndef pp_h
+#define pp_h
+
+#include <stdio.h>
+
+#endif /* pp_h */
