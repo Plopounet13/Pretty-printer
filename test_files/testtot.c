cc798cdf0760020a84e74b843d295308ff6d0d7e
Nicolas Derumigny <nderumigny@gmail.com>
2016-01-10 16:12:50 +0100

    New Examples

--- test_files/testtot.c
+++ test_files/testtot.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #if !(defined __LP64__ || defined __LLP64__) || defined _WIN32 && !defined _WIN64
-void bit(){for(int i=1; i<=32; i++){if (i%32){printf("32 bits\n");}}}// compilation pour un systeme 32-bit
+void bit(){for(int i=1; i<=32; i++){if (i%32){printf("32 bits\n");}}}/* compilation pour un systeme 32-bit */
 #else
-void bit(){for(int i=1; i<=32; i++){if (i%32){printf("64 bits\n");}}}// compilation pour un systeme 64-bit
+void bit(){for(int i=1; i<=32; i++){if (i%32){printf("64 bits\n");}}}/* compilation pour un systeme 64-bit */
 #endif
 #define ACVJ B
 
